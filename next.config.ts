8334692b64a1df08cdc5f89d76cc2d87dfc67144
Reut Ezra <reut.ezra@mail.huji.ac.il>
2026-04-20 19:20:35 +0300

    increase server action body limit for file attachments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActionsBodySizeLimit: '25mb',
+  },
 };
 
 export default nextConfig;
